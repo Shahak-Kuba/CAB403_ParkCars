20b7db1809a124cb3f49db92e9587ecca4289f8f
Shahak-Kuba <shahakuba98@gmail.com>
2021-10-26 17:16:41 +1000

    LPR detect from hash table implemented

--- manager.c
+++ manager.c
@@ -17,17 +17,22 @@ bool sim = true;
 // Predefining functions
 
 // hash table allocation
-int LPR_to_htab(htab_t* h);
+bool htab_init(htab_t *h, size_t n);
 size_t htab_index(htab_t *h, char *key);
 size_t djb_hash(char *s);
 void item_print(item_t *i);
+bool htab_add(htab_t *h, char *key, int value);
+item_t *htab_bucket(htab_t *h, char *key);
+int LPR_to_htab(htab_t* h);
+item_t *htab_find(htab_t *h, char *key);
+
 
 /* SHARED MEMORY functions */
 int shared_mem_init_open(shm_CP_t* shm, const char* shm_key);
 
 /* ENTRANCE functions */
 void enter_carpark();// this will be a function that allows a car to enter the carpark
-bool LPR_detect(char LPR[6]); // function that will check if the LPR is on the list
+bool LPR_detect(htab_t *h, char LPR[6]); // function that will check if the LPR is on the list
 void detect(char LPR[6]);// this function will verify that the car can enter the car park
 void navigate();// this function will navigate the car to the appropriate floor based on how full the carpark is
 void boom_control();// function to control boomgate status
@@ -52,35 +57,17 @@ int shared_mem_init_open(shm_CP_t* shm, const char* shm_key)
 
 
 
-bool LPR_detect(char *LPR)
+bool LPR_detect(htab_t *h, char *LPR)
 {
-    int line_num = 1;
-	char temp[512]; // file buffer
-    FILE *fptr = fopen("plates.txt", "r"); 
-    printf("%s\n",LPR);
-    if(fptr == NULL)
+    if(htab_find(h, LPR) == NULL)
     {
-        printf("Could not open plates.txt file. Please make sure it's in the same directory");
+        return false;
     }
-
-    while(fgets(temp, 512, fptr) != NULL) {
-		if((strstr(temp, LPR)) != NULL) {
-			return true; // licence found, return true
-		}
-		line_num++;
-	}
-
-    return false;
+    return true;
 }
 
 
 // -----------------------------------------------------Hash Table Function--------------------------------------------------------------------------
-
-// printing a item in the hash table
-void item_print(item_t *i)
-{
-    printf("key=%s value=%d", i->key, i->value);
-}
 
 // initialising has table
 bool htab_init(htab_t *h, size_t n)
@@ -123,6 +110,7 @@ bool htab_add(htab_t *h, char *key, int value)
     size_t bucket = htab_index(h, key);
     newhead->next = h->buckets[bucket];
     h->buckets[bucket] = newhead;
+    //printf("bucket %ld has key is: %s with a value of %d\n", bucket, h->buckets[bucket]->key, h->buckets[bucket]->value);
     return true;
 }
 
@@ -151,41 +139,13 @@ item_t *htab_find(htab_t *h, char *key)
     return NULL;
 }
 
-// Print the hash table.
-// pre: true
-// post: hash table is printed to screen
-void htab_print(htab_t *h)
-{
-    printf("hash table with %ld buckets\n", h->size);
-    for (size_t i = 0; i < h->size; ++i)
-    {
-        printf("bucket %ld: ", i);
-        if (h->buckets[i] == NULL)
-        {
-            printf("empty\n");
-        }
-        else
-        {
-            for (item_t *j = h->buckets[i]; j != NULL; j = j->next)
-            {
-                item_print(j);
-                if (j->next != NULL)
-                {
-                    printf(" -> ");
-                }
-            }
-            printf("\n");
-        }
-    }
-}
-
 // reading from file directly into a hash table
 int LPR_to_htab(htab_t *h)
 {
     // initialising hast table
     // Reading the file
     // initialising buffer of 100000 bytes (in case of a long plates.txt)
-    char source[8]; // reading a number plate of 8 bytes long
+    char source[6]; // reading a number plate of 8 bytes long
     FILE *file = fopen(LPFILE, "r"); // opening in file mode
     // checking file exists
     if (file == NULL)
@@ -196,7 +156,7 @@ int LPR_to_htab(htab_t *h)
     while((fscanf(file, "%s", source)) != EOF)
     {
         //printf("%s\n", source);
-        if (htab_add(h, source, 0) == false)
+        if ((htab_add(h, source, 0)) == false)
         {
             printf("error adding number plate to hash table");
         }
@@ -219,7 +179,7 @@ int main()
     
 
     // initialising has table
-    size_t buckets = 25;
+    size_t buckets = 5;
     htab_t h;
     if (!htab_init(&h, buckets))
     {
