6c532eef462402301c9cd9c5d4b33d0091aad195
Shaq <n9996095@qut.edu.au>
2021-10-28 19:19:09 +1000

    We gettting cars in yay!!!

--- manager.c
+++ manager.c
@@ -13,9 +13,11 @@
 // Global variables
 shm_CP_t CP;
 htab_t h;
-/*
+
 // level counter to count how many cars in a level
 int level_car_counter[] = {0,0,0,0,0}; // zero cars in each level initally
+pthread_mutex_t level_car_counter_mutex;
+/*2
 // creating global linked list
 Car_t *head[5];
 for (int i = 0; i < 5; i++)
@@ -53,6 +55,12 @@ int shared_mem_init_open(shm_CP_t* shm, const char* shm_key)
             perror("unable to open shared memory");
             return(EXIT_FAILURE);
         }
+    // mapping memory
+    if((shm->shm_ptr = mmap(0, SHMSZ, PROT_WRITE, MAP_SHARED, shm->fd,0)) == (CP_t *) - 1)
+    {
+        perror("mmap");
+        return(EXIT_FAILURE);
+    }
     printf("shared memory opened!\n");
     return(EXIT_SUCCESS);
 }
@@ -209,7 +217,75 @@ void htab_print(htab_t *h)
 
 // Enternce routine
 
+void* enterFunc(void* enter_num)
+{
+    // get the entrance we are looking at
+    int num = *(int *)enter_num;
+    Enter_t *entrance = &CP.shm_ptr->Enter[num];
+    // initially locking all mutex's for enterance 
+    pthread_mutex_lock(&entrance->LPR_mutex);
+    // check if car arrives at entrance gate
+    while(1)
+    {
+        printf("waiting for signal....\n");
+        
+        if(entrance->LPR_reading[0] != 0){
+            char temp_LPR[7];
+            // copy the number plate from car arrived at gate
+            memcpy(temp_LPR, entrance->LPR_reading, 6);
+            temp_LPR[6] = 0;
+            // check if car is allowed in
+            if(htab_find(&h, temp_LPR) != NULL){
+                // find level number and incrementing
+                pthread_mutex_lock(&level_car_counter_mutex);
+                int level_num = -1;
+                for (int i = 0; i < NUM_LEVELS; i++)
+                {
+                    if(level_car_counter[i] < LEVEL_CAPACITY)
+                    {
+                        level_num = i;
+                        level_car_counter[i]++;
+                        break;
+                    }
+                }
+                // check if carpark is full
+                if(level_num == -1)
+                {
+                    printf("Carpark is full idiot!");
+                    break;
+                }
 
+
+                // release level counter mutex 
+                pthread_mutex_unlock(&level_car_counter_mutex);
+                printf("%s inserted into carpark at level %d\n", temp_LPR, level_num);
+                // sending signal and updating status
+                pthread_mutex_lock(&entrance->BOOM_mutex);
+                entrance->BOOM_status = 'R';
+                pthread_mutex_unlock(&entrance->BOOM_mutex);
+                // sending signal to simulator boom
+                pthread_cond_signal(&entrance->BOOM_cond);
+
+                // find/ allocate the car to a level
+                Level_t *level = &CP.shm_ptr->Level[level_num]; 
+                pthread_mutex_lock(&level->LPR_mutex);
+                memcpy(level->LPR_reading,temp_LPR,6);
+                pthread_mutex_unlock(&level->LPR_mutex);
+                
+            }
+        }
+        pthread_cond_wait(&entrance->LPR_cond, &entrance->LPR_mutex);
+        printf("recieved signal\n");
+    }
+    //unlocking all mutex
+    pthread_mutex_unlock(&entrance->LPR_mutex);
+    return 0;
+
+}
+
+// -----------------------------------------------------Entrance Function--------------------------------------------------------------------------
+
+// Level routine
 
 // -----------------------------------------------------------------------------------------------------------------MAIN--------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -222,7 +298,6 @@ int main()
 
     // initialising has table
     size_t buckets = 10;
-    htab_t h;
     if (!htab_init(&h, buckets))
     {
         printf("failed to initialise hash table\n");
@@ -232,9 +307,23 @@ int main()
     LPR_to_htab(&h);
     htab_print(&h);
 
+    printf("SENDING IT.....\n");
+    char LPR[7] = "927KOB";
+    printf("%s \n", LPR);
 
-    
+    memcpy(CP.shm_ptr->Enter[0].LPR_reading, LPR, 6);
 
+    int level_num = 0;
+
+    pthread_t enter; 
+    pthread_create(&enter,NULL, enterFunc , (void *)&level_num);
+    sleep(1);
+    pthread_mutex_lock(&CP.shm_ptr->Enter[0].LPR_mutex);
+    pthread_cond_signal(&CP.shm_ptr->Enter[0].LPR_cond);
+    pthread_mutex_unlock(&CP.shm_ptr->Enter[0].LPR_mutex);
+
+    int* ptr;
+    pthread_join(enter, (void**)&ptr);
 
     return EXIT_SUCCESS;
 }
