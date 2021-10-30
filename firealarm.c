b210b1e96ede769a439f9f438f34c68b4a883486
Jon <jonpatonmedia@gmail.com>
2021-10-30 21:27:11 +1000

    Cleaned Code, Need To Do Exit Cleanup

--- firealarm.c
+++ firealarm.c
@@ -5,50 +5,41 @@ shm_CP_t shm;
 int alarm_active = 0;
 int shared_mem_init_open(shm_CP_t *shm, const char *shm_key);
 
-#define LEVELS 5
-#define ENTRANCES 5
-#define EXITS 5
-
 #define MEDIAN_WINDOW 5
 #define TEMPCHANGE_WINDOW 30
 
-int16_t tempArray [LEVELS][TEMPCHANGE_WINDOW + MEDIAN_WINDOW]; // assume that 0 values are init
-int tempArrayIndex[LEVELS];
+int16_t tempArray [NUM_LEVELS][TEMPCHANGE_WINDOW + MEDIAN_WINDOW]; // assume that 0 values are init
+int tempArrayIndex[NUM_LEVELS];
 
-typedef struct {
-	pthread_mutex_t m;
-	pthread_cond_t c;
-	char s;
-} boomgate;
-
-struct parkingsign {
-	pthread_mutex_t m;
-	pthread_cond_t c;
-	char display;
-};
-
-
-int compare(const void *first, const void *second)
-{
-	return *((const int *)first) - *((const int *)second);
+//memory initialisation
+int shared_mem_init_open(shm_CP_t* shm, const char *shm_key)
+{   
+    // opening the shared data, otherwise producing an error
+    if ((shm->fd = shm_open(shm_key, O_RDWR, 0)) < 0) {
+            return(EXIT_FAILURE);
+        }
+    // mapping memory
+    if((shm->shm_ptr = mmap(0, SHMSZ, PROT_WRITE, MAP_SHARED, shm->fd,0)) == (CP_t *) - 1) {
+        return(EXIT_FAILURE);
+    }
+    //memory sucessfully shared
+    return(EXIT_SUCCESS);
 }
-
 
 // function to sort the array in ascending order
 int16_t * sort(int16_t *array , int n)
 { 
     // declare some local variables
-    int i=0 , j=0 , temp=0;
+    int i = 0;
+    int j = 0;
+    int temp = 0;
 
-    for(i=0 ; i<n ; i++)
-    {
-        for(j=0 ; j<n-1 ; j++)
-        {
-            if(array[j]>array[j+1])
-            {
-                temp        = array[j];
-                array[j]    = array[j+1];
-                array[j+1]  = temp;
+    for(i=0 ; i<n ; i++) {
+        for(j=0 ; j<n-1 ; j++) {
+            if(array[j]>array[j+1]) {
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
             }
         }
     }
@@ -56,25 +47,25 @@ int16_t * sort(int16_t *array , int n)
 }
 
 // function to calculate the median of the array
-int16_t get_median(int16_t array[] , int n)
-{
+int16_t get_median(int16_t array[] , int n) {
     int16_t median=0;
     
-    // if number of elements are even
-    if(n%2 == 0)
+    if(n%2 == 0) {
+        //even number of elements in inputted array
         median = (array[(n-1)/2] + array[n/2])/2.0;
-    // if number of elements are odd
-    else
+    } else {
+        //odd number of elements in inputted array
         median = array[n/2];
-    
+    }
     return median;
 }
 
+//temperature monitoring and evaluating for a specific level
 void tempmonitor(int level) {
     int mediantemp = 0; //Resultant temp from smoothing process
     int hightemps = 0; //Count number of high temps from last 30 readings
-    int16_t temp = 0;
-    int16_t buffer[MEDIAN_WINDOW];
+    int16_t temp = 0; //temperature taken from sensor
+    int16_t buffer[MEDIAN_WINDOW]; //local array to be destructively changed to find median
     
     temp = shm.shm_ptr->Level[level].temp_sensor; //take value currently stored in temp
 
@@ -107,8 +98,7 @@ void tempmonitor(int level) {
     }
     // If 90% of the last 30 temperatures are >= 58 degrees,
     // this is considered a high temperature. Raise the alarm
-    //printf("High Temp: %d\n", hightemps);
-    if (hightemps >= TEMPCHANGE_WINDOW * 0.9) {
+    if (hightemps >= TEMPCHANGE_WINDOW * 0.9 && tempArray[level][TEMPCHANGE_WINDOW+MEDIAN_WINDOW-1] != 0) {
         alarm_active = 1;
     }
 
@@ -120,79 +110,21 @@ void tempmonitor(int level) {
     }
 }
 
-/* Dummy Function For Spitting Out Temperatures To Console....
-void dumpTempMemory() {
-    //testing printout
-    printf("Temp Memory Block:\n");
-    for (int x = 0; x < LEVELS; x++) {
-        for (int y = 0; y < TEMPCHANGE_WINDOW + MEDIAN_WINDOW; y++) {
-            printf("%d ", tempArray[x][y]);
-            if (y == 4) {
-                printf(" | ");
-            }
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-*/
-
-int shared_mem_init_open(shm_CP_t* shm, const char *shm_key)
-{   
-    // opening the shared data, otherwise producing an error
-    if ((shm->fd = shm_open(shm_key, O_RDWR, 0)) < 0)
-        {
-            perror("unable to open shared memory");
-            return(EXIT_FAILURE);
-        }
-    // mapping memory
-    if((shm->shm_ptr = mmap(0, SHMSZ, PROT_WRITE, MAP_SHARED, shm->fd,0)) == (CP_t *) - 1)
-    {
-        perror("mmap");
-        return(EXIT_FAILURE);
-    }
-    printf("shared memory opened!\n");
-    return(EXIT_SUCCESS);
-}
-
-void *openboomgate(boomgate *bg)
-{
-	pthread_mutex_lock(&bg->m);
-	for (;;) {
-		if (bg->s == 'C') {
-			bg->s = 'R';
-			pthread_cond_broadcast(&bg->c);
-		}
-		if (bg->s == 'O') {
-		}
-		pthread_cond_wait(&bg->c, &bg->m);
-	}
-	pthread_mutex_unlock(&bg->m);
-	
-}
-
-int main(void)
-{
+int main(void) {
     const char* key;
     key = KEY;
     shared_mem_init_open(&shm, key);
 
-    while(alarm_active == 0)
-    {
-        for (int lvl = 0; lvl < LEVELS; lvl++) 
-        {
+    while(alarm_active == 0) {
+        for (int lvl = 0; lvl < NUM_LEVELS; lvl++) {
             tempmonitor(lvl); 
         }
-        //sleep
         usleep(2000); //wait 2ms between next reading
     }
-	
-	//alarm flag true, raise alarm:
-	fprintf(stderr, "*** ALARM ACTIVE ***\n");
-	
-	// Handle the alarm system and open boom gates
+	//alarm flag true, raise alarm.
+
 	// Activate alarms on all levels
-    for (int i = 0; i < LEVELS; i++) {
+    for (int i = 0; i < NUM_LEVELS; i++) {
         pthread_mutex_lock(&shm.shm_ptr->Level[i].LPR_mutex); //lock level
         shm.shm_ptr->Level[i].fire_alarm = (char)1; //set alarm flag
         pthread_cond_broadcast(&shm.shm_ptr->Level[i].LPR_cond); //send condition
@@ -200,41 +132,45 @@ int main(void)
     }
 	
 	// Open up all boom gates
-	for (int i = 0; i < EXITS; i++) {
+    //open exits
+	for (int i = 0; i < NUM_EXITS; i++) {
         char gateState = shm.shm_ptr->Exit[i].BOOM_status;
-        if (gateState == 'C') {
-            pthread_mutex_lock(&shm.shm_ptr->Exit[i].BOOM_mutex); //lock exit
+        pthread_mutex_lock(&shm.shm_ptr->Exit[i].BOOM_mutex); //lock exit
+        if (gateState == 'C') { //if gate already open dont need to change.
             shm.shm_ptr->Exit[i].BOOM_status = 'R'; //set gate to raise
             pthread_cond_broadcast(&shm.shm_ptr->Exit[i].BOOM_cond); //send condition
-            pthread_mutex_unlock(&shm.shm_ptr->Exit[i].BOOM_mutex); //unlock exit
         }
+        pthread_mutex_unlock(&shm.shm_ptr->Exit[i].BOOM_mutex); //unlock exit
     }
-	
+    //open entrances
+    for (int i = 0; i < NUM_ENTERS; i++) {
+        char gateState = shm.shm_ptr->Enter[i].BOOM_status;
+        pthread_mutex_lock(&shm.shm_ptr->Enter[i].BOOM_mutex); //lock exit
+        if (gateState == 'C') { //if gate already open dont need to change.
+            shm.shm_ptr->Enter[i].BOOM_status = 'R'; //set gate to raise
+            pthread_cond_broadcast(&shm.shm_ptr->Enter[i].BOOM_cond); //send condition
+        }
+        pthread_mutex_unlock(&shm.shm_ptr->Enter[i].BOOM_mutex); //unlock exit
+    }	
+
 	// Show evacuation message on an endless loop
     char *evacmessage = "EVACUATE ";
 	for (;;) {
 		for (char *p = evacmessage; *p != '\0'; p++) {
             char letter = (char)p[0];
-            //printf("%c\n",letter);
 
             //Display Evacuate on Entrace Signs
-            for (int i = 0; i < ENTRANCES; i++) {
+            for (int i = 0; i < NUM_ENTERS; i++) {
                 pthread_mutex_lock(&shm.shm_ptr->Enter[i].info_sign_mutex); //lock entrance
                 shm.shm_ptr->Enter[i].info_sign_status = letter; //set sign character
                 pthread_cond_broadcast(&shm.shm_ptr->Enter[i].info_sign_cond); //send condition
                 pthread_mutex_unlock(&shm.shm_ptr->Enter[i].info_sign_mutex); //unlock level mutex
             }
-
-			//for (int i = 0; i < ENTRANCES; i++) {
-			//	int addr = 288 * i + 192;
-			//	volatile struct parkingsign *sign = &shm + addr;
-			//	pthread_mutex_lock(&sign->m);
-			//	sign->display = *p;
-			//	pthread_cond_broadcast(&sign->c);
-			//	pthread_mutex_unlock(&sign->m);
-			//}
 			usleep(20000); //sleep for 20ms between letters
 		}
 	}
+    //clean exit
+    //shm_unlink(&shm);
+    //munmap(&shm,SHMSZ);
     return EXIT_SUCCESS;
 }
